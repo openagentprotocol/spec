f3fdeaf6f618feca729ee49ad050fa2907bd2e59
riccardone <riccardo@dinuzzo.it>
2026-04-08 13:57:40 +0100

    fix config

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,9 +3,5 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	define: {
-		'import.meta.env.VITE_GIT_SHA': JSON.stringify(process.env.VITE_GIT_SHA ?? ''),
-		'import.meta.env.VITE_BUILD_TIME': JSON.stringify(process.env.VITE_BUILD_TIME ?? '')
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
